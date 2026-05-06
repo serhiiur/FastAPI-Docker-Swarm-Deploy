d9d46c1d594759e4e1d83d4727ae7269cf0e7bc2
serhiiur <serhiiur@duck.com>
2026-05-06 17:24:20 +0300

    Fix indentations

--- src/app/core/models.py
+++ src/app/core/models.py
@@ -5,11 +5,11 @@ from sqlmodel import Field, SQLModel
 
 
 class Base(SQLModel):
-  """Base declarative SQL model."""
+    """Base declarative SQL model."""
 
-  id: int | None = Field(default=None, primary_key=True)
-  created_at: datetime = Field(default_factory=func.now)
-  updated_at: datetime = Field(
-    default_factory=func.now,
-    sa_column_kwargs={"onupdate": func.now},
-  )
+    id: int | None = Field(default=None, primary_key=True)
+    created_at: datetime = Field(default_factory=func.now)
+    updated_at: datetime = Field(
+        default_factory=func.now,
+        sa_column_kwargs={"onupdate": func.now},
+    )
