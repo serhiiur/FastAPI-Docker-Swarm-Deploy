9f1b70bf6c8643402f5f728c64e592f0c4e82589
serhiiur <serhiiur@duck.com>
2026-05-06 17:30:38 +0300

    Remove redundant pytestmark

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,29 +1,26 @@
 from typing import TYPE_CHECKING
 
-import pytest
 from fastapi import status
 
 from app.core.schemas import ApiHealth, ApiVersion
 
 if TYPE_CHECKING:
-  from httpx import AsyncClient
-
-pytestmark = pytest.mark.anyio
+    from httpx import AsyncClient
 
 
 async def test_health(client: "AsyncClient") -> None:
-  resp = await client.get("/api/health")
-  assert resp.status_code == status.HTTP_200_OK
-  api_health_schema = ApiHealth()
-  resp_json = resp.json()
-  assert resp_json.get("status") == api_health_schema.status
-  assert "timestamp" in resp_json
+    resp = await client.get("/api/health")
+    assert resp.status_code == status.HTTP_200_OK
+    api_health_schema = ApiHealth()
+    resp_json = resp.json()
+    assert resp_json.get("status") == api_health_schema.status
+    assert "timestamp" in resp_json
 
 
 async def test_version(client: "AsyncClient") -> None:
-  resp = await client.get("/api/version")
-  assert resp.status_code == status.HTTP_200_OK
-  api_version_schema = ApiVersion()
-  resp_json = resp.json()
-  assert resp_json.get("version") == api_version_schema.version
-  assert "timestamp" in resp_json
+    resp = await client.get("/api/version")
+    assert resp.status_code == status.HTTP_200_OK
+    api_version_schema = ApiVersion()
+    resp_json = resp.json()
+    assert resp_json.get("version") == api_version_schema.version
+    assert "timestamp" in resp_json
