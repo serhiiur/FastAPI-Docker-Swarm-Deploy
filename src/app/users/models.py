8a23c6c949494141a8ec179aafb2526b4dffe3f8
serhiiur <serhiiur@duck.com>
2026-05-06 17:29:50 +0300

    Fix indentations

--- src/app/users/models.py
+++ src/app/users/models.py
@@ -4,4 +4,4 @@ from .schemas import CreateUser
 
 
 class User(Base, CreateUser, table=True):
-  """Database model to represent a user in the database."""
+    """Database model to represent a user in the database."""
