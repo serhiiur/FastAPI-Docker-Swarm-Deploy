6f15adfcfde89c049ddf7c22a44498ee629bad1c
serhiiur <serhiiur@duck.com>
2026-05-06 17:27:06 +0300

    Remove unnecessary objects from lifespan

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,5 @@
-import logging
 from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
-from typing import TypedDict
 
 from fastapi import FastAPI
 
@@ -11,23 +9,18 @@ from app.core.settings import settings
 from app.users.routes import router as user_router
 
 
-class AppState(TypedDict):
-  """Data structure to represent state of the main application."""
-
-  logger: "logging.Logger"
-
-
 @asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncIterator[AppState]:
-  """Run database migrations and define application state objects."""
-  logger = getattr(app.state, "logger", logging.getLogger(settings.logger_name))
-  yield AppState(logger=logger)
+async def lifespan(_: FastAPI) -> AsyncIterator[None]:
+    """Run database migrations and define application state objects."""
+    # startup actions
+    yield
+    # shutdown actions
 
 
 app = FastAPI(
-  **settings.fastapi_kwargs,
-  exception_handlers=error_handlers,
-  lifespan=lifespan,
+    **settings.fastapi.model_dump(),
+    exception_handlers=error_handlers,
+    lifespan=lifespan,
 )
 app.include_router(core_router)
 app.include_router(user_router)
