2557b766e7fffb5459e8a66350666b0c0d548eac
serhiiur <serhiiur@duck.com>
2026-05-06 17:29:08 +0300

    Fix indentations

--- src/app/users/routes.py
+++ src/app/users/routes.py
@@ -6,10 +6,10 @@ from .models import User
 from .schemas import CreateUser, UpdateUser
 
 router = crud_router(
-  session=get_db_session,
-  model=User,
-  create_schema=CreateUser,
-  update_schema=UpdateUser,
-  path="/api/users",
-  tags=["users"],
+    session=get_db_session,
+    model=User,
+    create_schema=CreateUser,
+    update_schema=UpdateUser,
+    path="/api/users",
+    tags=["users"],
 )
