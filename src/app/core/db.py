85cb2a1628be49912b9ec5e26953dc0eab9cb5dc
serhiiur <serhiiur@duck.com>
2026-05-06 17:25:49 +0300

    Change path to settings

--- src/app/core/db.py
+++ src/app/core/db.py
@@ -2,5 +2,5 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from .settings import settings
 
-engine = create_async_engine(settings.database_url, echo=settings.debug)
+engine = create_async_engine(settings.db.url, echo=settings.fastapi.debug)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
