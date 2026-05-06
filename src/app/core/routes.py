5b4e5e5b4e0dde3d73108dda40856127337e5083
serhiiur <serhiiur@duck.com>
2026-05-06 17:26:06 +0300

    Fix indentations

--- src/app/core/routes.py
+++ src/app/core/routes.py
@@ -7,11 +7,11 @@ router = APIRouter(prefix="/api", tags=["internal"])
 
 @router.get("/version")
 async def version() -> ApiVersion:
-  """Return information about API version."""
-  return ApiVersion.model_construct()
+    """Return information about API version."""
+    return ApiVersion.model_construct()
 
 
 @router.get("/health")
 async def health() -> ApiHealth:
-  """Return information about API health status."""
-  return ApiHealth.model_construct()
+    """Return information about API health status."""
+    return ApiHealth.model_construct()
