8843ded8dad1f6f49f6d9e4e71f0dc6b19e9e98c
serhiiur <serhiiur@duck.com>
2026-05-07 10:32:50 +0300

    Remove unused dependencies

--- src/app/core/dependencies.py
+++ src/app/core/dependencies.py
@@ -1,32 +1,12 @@
-import logging
 from collections.abc import AsyncIterator
 from typing import TYPE_CHECKING, Annotated, TypeAlias
 
 from fastapi import Depends
 
 from .db import async_session
-from .settings import settings
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
-
-    from .settings import Settings as AppSettings
-
-
-async def get_settings() -> "AppSettings":
-    """Return settings object to be used as a dependency."""
-    return settings
-
-
-Settings: TypeAlias = Annotated["AppSettings", Depends(get_settings)]
-
-
-async def get_logger(settings: Settings) -> logging.Logger:
-    """Return logger object initialized in the lifespan."""
-    return logging.getLogger(settings.logger.name)
-
-
-Logger: TypeAlias = Annotated[logging.Logger, Depends(get_logger)]
 
 
 async def get_db_session() -> AsyncIterator["AsyncSession"]:
